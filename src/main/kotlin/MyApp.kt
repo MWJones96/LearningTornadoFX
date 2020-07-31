038ab8e1fe142b09599884d219b4a26ea3c5a7bd
Matthew Jones <jonesmat@usc.edu>
2020-07-30 23:37:31 -0700

    More tutorial stuff

--- src/main/kotlin/MyApp.kt
+++ src/main/kotlin/MyApp.kt
@@ -1,30 +1,45 @@
 import javafx.beans.property.SimpleStringProperty
 import javafx.collections.FXCollections
+import javafx.scene.Parent
+import javafx.stage.StageStyle
 import tornadofx.*
 
-class MyApp: App(MyView::class)
+class MyApp: App(MyView1::class)
 
-class MyView: View() {
-    val controller: MyController by inject()
-
+class MyView1: View() {
     override val root = vbox {
-        val textField = textfield()
-        button("Update Text") {
+        button("Go to View2") {
             action {
-                runAsync {
-                    controller.loadText()
-                } ui { loadedText ->
-                    textField.text = loadedText
-                }
+                replaceWith(MyView2::class, ViewTransition.Slide(0.3.seconds, ViewTransition.Direction.RIGHT))
             }
         }
     }
+
+    override fun onDock() {
+        println("Docking View1")
+    }
+
+    override fun onUndock() {
+        println("Undocking View1")
+    }
 }
 
-class MyController: Controller() {
-    fun loadText(): String {
-        return "Asynchronous text"
+class MyView2: View() {
+    override val root = vbox {
+        button("Go to View1") {
+            action {
+                replaceWith(MyView1::class, ViewTransition.Slide(0.3.seconds, ViewTransition.Direction.LEFT))
+            }
+        }
     }
+    override fun onDock() {
+        println("Docking View2")
+    }
+
+    override fun onUndock() {
+        println("Undocking View2")
+    }
+
 }
 
 fun main(args: Array<String>) {
